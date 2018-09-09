185571063e0f6206495f038f622a45b1aa4a42a1
Mark Glushko <markableglushko@gmail.com>
2018-09-09 15:26:42 +0300

    Передача в фрагменты через adapter

--- app/src/main/java/com/example/markable/footballapptest/Adapters/SampleFragmentPageAdapter.java
+++ app/src/main/java/com/example/markable/footballapptest/Adapters/SampleFragmentPageAdapter.java
@@ -20,17 +20,21 @@ public class SampleFragmentPageAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 3;
     private String tabTitles[] = new String[]{"Таблица", "Результаты", "Календарь"};
     private Context context;
+    private String divTable;
+    private String prevReslts;
 
-    public SampleFragmentPageAdapter(FragmentManager fm, Context context) {
+    public SampleFragmentPageAdapter(FragmentManager fm, Context context, String divTable, String prevResults) {
         super(fm);
         this.context = context;
+        this.divTable = divTable;
+        this.prevReslts = prevResults;
     }
 
     @Override
     public Fragment getItem(int position) {
         switch (position){
-            case 0: return new FragmentForTable().newInstance("Вкладка " + position);
-            case 1: return new FragmentForResults().newInstance("Вкладка " + position);
+            case 0: return new FragmentForTable().newInstance("Вкладка " + position + " " + divTable);
+            case 1: return new FragmentForResults().newInstance("Вкладка " + position + " " + prevReslts);
             case 2: return new FragmentForCalendar().newInstance("Вкладка " + position);
             default: return new FragmentForTable().newInstance("Вкладка " + position);
         }
