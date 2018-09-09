db9eea33f8eb8264bed6cf0984e2bc3bb339c955
Mark Glushko <markableglushko@gmail.com>
2018-09-09 15:26:21 +0300

    Добавил загрузочный класс в главный фрагмент

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentMain.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentMain.java
@@ -1,5 +1,6 @@
 package com.example.markable.footballapptest.Fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,10 +14,64 @@ import android.view.ViewGroup;
 
 import com.example.markable.footballapptest.Adapters.SampleFragmentPageAdapter;
 import com.example.markable.footballapptest.R;
+import com.google.gson.Gson;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class FragmentMain extends Fragment {
 
     private static final String TAG = "FragMain";
+
+    private String prevResults = "", table = "";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public class ServerConnectTest extends AsyncTask<String, Void, String>{
+
+        String query = "{\"id_division\":1,\"id_tour\":2}";
+        String fromServer = null, fromServerResultsPrevMatches = null ;
+        String ipAdres = "192.168.0.100";
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            Log.i(TAG, "Поток запущен");
+            Socket socket;
+            Gson gson = new Gson();
+            try {
+                socket = new Socket(ipAdres, 55555);
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                DataInputStream inResultsPrev = new DataInputStream((socket.getInputStream()));
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                out.writeUTF(query);
+
+                fromServer = in.readUTF();
+                table = fromServer;
+                Log.i(TAG, "Данные с сервера в виду JSON = " + fromServer);
+                fromServerResultsPrevMatches = inResultsPrev.readUTF();
+                prevResults = fromServerResultsPrevMatches;
+                Log.i(TAG,"[2] Данные с сервера в виде JSON = " + fromServerResultsPrevMatches);
+                out.close();
+                in.close();
+                inResultsPrev.close();
+                socket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+    }
 
     @Nullable
     @Override
@@ -25,10 +80,19 @@ public class FragmentMain extends Fragment {
 
         Log.i(TAG, "OnCreateView: Загрузка главного фрагмента");
 
-        ViewPager viewPager = view.findViewById(R.id.viewPager);
-        viewPager.setAdapter(new SampleFragmentPageAdapter(getChildFragmentManager(), getContext()));
-        TabLayout tabLayout = view.findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        new ServerConnectTest().execute();
+        int i = 0;
+        while (table.equals("")){
+            i++;
+        }
+        Log.i(TAG, String.valueOf(i));
+        if (!table.equals("")){
+            Log.i(TAG, "Зашел в if. Поля основного класса = " + table + prevResults);
+            ViewPager viewPager = view.findViewById(R.id.viewPager);
+            viewPager.setAdapter(new SampleFragmentPageAdapter(getChildFragmentManager(), getContext(), table, prevResults));
+            TabLayout tabLayout = view.findViewById(R.id.sliding_tabs);
+            tabLayout.setupWithViewPager(viewPager);
+        }
 
         return view;
     }
