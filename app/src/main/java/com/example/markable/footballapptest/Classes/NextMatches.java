5b6f055ae3b62c8849a1065d63d20d57279f16ff
Mark Glushko <markableglushko@gmail.com>
2018-10-02 14:48:18 +0300

    Добавил нужные поля в новый класс

--- app/src/main/java/com/example/markable/footballapptest/Classes/NextMatches.java
+++ app/src/main/java/com/example/markable/footballapptest/Classes/NextMatches.java
@@ -1,4 +1,58 @@
 package com.example.markable.footballapptest.Classes;
 
-public class NextMatches {
+import java.io.Serializable;
+
+public class NextMatches implements Serializable {
+
+    private String nameDivision;
+    private int idTour;
+    private String teamHome;
+    private String teamVisit;
+    private String date;
+    private String nameStadium;
+
+    public NextMatches(String nameDivision, int idTour, String teamHome, String teamVisit, String date, String nameStadium) {
+        this.nameDivision = nameDivision;
+        this.idTour = idTour;
+        this.teamHome = teamHome;
+        this.teamVisit = teamVisit;
+        this.date = date;
+        this.nameStadium = nameStadium;
+    }
+
+    public String getNameDivision() {
+        return nameDivision;
+    }
+
+    public int getIdTour() {
+        return idTour;
+    }
+
+    public String getTeamHome() {
+        return teamHome;
+    }
+
+    public String getTeamVisit() {
+        return teamVisit;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getNameStadium() {
+        return nameStadium;
+    }
+
+    @Override
+    public String toString() {
+        return "NextMatches{" +
+                "nameDivision='" + nameDivision + '\'' +
+                ", idTour=" + idTour +
+                ", teamHome='" + teamHome + '\'' +
+                ", teamVisit='" + teamVisit + '\'' +
+                ", date='" + date + '\'' +
+                ", nameStadium='" + nameStadium + '\'' +
+                "}\n";
+    }
 }
