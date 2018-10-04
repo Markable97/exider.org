dcd8ace8b1c01cf1aa6a6cfda138bac71946273e
Mark Glushko <markableglushko@gmail.com>
2018-10-04 18:38:31 +0300

    добавил геттеры и конструктор для класса Картинок. Реализовал интерфейс для сериализации

--- app/src/main/java/com/example/markable/footballapptest/Classes/ImageFromServer.java
+++ app/src/main/java/com/example/markable/footballapptest/Classes/ImageFromServer.java
@@ -1,4 +1,24 @@
 package com.example.markable.footballapptest.Classes;
 
-public class ImageFromServer {
+import android.graphics.Bitmap;
+
+import java.io.Serializable;
+
+public class ImageFromServer implements Serializable {
+
+    String nameImage;
+    Bitmap bitmapImage;
+
+    public ImageFromServer(String nameImage, Bitmap bitmapImage) {
+        this.nameImage = nameImage;
+        this.bitmapImage = bitmapImage;
+    }
+
+    public String getNameImage() {
+        return nameImage;
+    }
+
+    public Bitmap getBitmapImage() {
+        return bitmapImage;
+    }
 }
