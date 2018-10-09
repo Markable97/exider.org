d842aedb7d4258ac12d9baf0202167fe1bf89aa2
Mark Glushko <markableglushko@gmail.com>
2018-10-09 20:04:26 +0300

    Добавление разметки в классы

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticSquadList.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticSquadList.java
@@ -1,4 +1,22 @@
 package com.example.markable.footballapptest.Fragments;
 
-public class FragmentStatisticSquadList {
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.markable.footballapptest.R;
+
+public class FragmentStatisticSquadList extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_statistic_squad, container, false);
+
+        return view;
+    }
 }
