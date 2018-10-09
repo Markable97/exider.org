09c00871c57df113ada2b32b357c6a7e6a07ce56
Mark Glushko <markableglushko@gmail.com>
2018-10-09 19:26:29 +0300

    Создание программирование (java + xml)

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamStatistic.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamStatistic.java
@@ -1,4 +1,23 @@
 package com.example.markable.footballapptest.Fragments;
 
-public class FragmentForTeamStatistic {
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.markable.footballapptest.R;
+
+public class FragmentForTeamStatistic extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_team_statistic, container, false);
+
+        return view;
+    }
 }
