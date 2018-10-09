5915e29370e35cf5a69503a7441a45dfb354a498
Mark Glushko <markableglushko@gmail.com>
2018-10-09 20:04:48 +0300

    подключение адаптера в главный класс для табов

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamStatistic.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamStatistic.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.markable.footballapptest.Adapters.FragmentPageAdapterForTeam;
 import com.example.markable.footballapptest.R;
 
 public class FragmentForTeamStatistic extends Fragment {
@@ -18,12 +19,17 @@ public class FragmentForTeamStatistic extends Fragment {
     ViewPager viewPager;
     TabLayout tabLayout;
 
+    FragmentPageAdapterForTeam mAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_team_statistic, container, false);
 
         viewPager = view.findViewById(R.id.viewPager_team_statistic);
         tabLayout = view.findViewById(R.id.statistic_tabs);
+        mAdapter = new FragmentPageAdapterForTeam(getChildFragmentManager());
+        viewPager.setAdapter(mAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
         return view;
     }
