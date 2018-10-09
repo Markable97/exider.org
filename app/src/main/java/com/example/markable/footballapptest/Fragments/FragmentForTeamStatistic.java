0dc9185cd6419e1ea0921ed88869fb363f4bf124
Mark Glushko <markableglushko@gmail.com>
2018-10-09 19:40:38 +0300

    Добавление разметки (ViewPager and TabLayout) и внесение в java код

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamStatistic.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamStatistic.java
@@ -2,7 +2,10 @@ package com.example.markable.footballapptest.Fragments;
 
 import android.os.Bundle;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +15,15 @@ import com.example.markable.footballapptest.R;
 public class FragmentForTeamStatistic extends Fragment {
 
 
-
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_team_statistic, container, false);
+
+        viewPager = view.findViewById(R.id.viewPager_team_statistic);
+        tabLayout = view.findViewById(R.id.statistic_tabs);
 
         return view;
     }
