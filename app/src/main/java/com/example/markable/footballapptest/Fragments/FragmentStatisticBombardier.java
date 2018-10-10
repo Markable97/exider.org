9dfc759520e77441bb5ea945d0649f7e50c7bf65
Mark Glushko <markableglushko@gmail.com>
2018-10-10 12:50:56 +0300

    изменение макетов для статистики по игрокам

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticBombardier.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticBombardier.java
@@ -5,10 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
 import com.example.markable.footballapptest.R;
 
 public class FragmentStatisticBombardier extends Fragment {
+
+    TableLayout table;
 
     public static FragmentStatisticBombardier newInstance(){
         FragmentStatisticBombardier fragment = new FragmentStatisticBombardier();
@@ -19,6 +22,7 @@ public class FragmentStatisticBombardier extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_statistic_bombardier, container, false);
+        table = view.findViewById(R.id.tableForBombardier);
 
         return view;
     }
