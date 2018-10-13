a6b542acbdf941fc7cd552a981ca66d4bdf2cfc0
Mark Glushko <markableglushko@gmail.com>
2018-10-13 11:18:10 +0300

    метод для общения

--- app/src/main/java/com/example/markable/footballapptest/ReturnFromFragForAct.java
+++ app/src/main/java/com/example/markable/footballapptest/ReturnFromFragForAct.java
@@ -1,4 +1,10 @@
 package com.example.markable.footballapptest;
 
+import com.example.markable.footballapptest.Classes.Player;
+
+import java.util.ArrayList;
+
 public interface ReturnFromFragForAct {
+
+    void sendDataToActivity(ArrayList<Player> arrayPlayers);
 }
