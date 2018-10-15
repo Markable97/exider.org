861c4542124df80ad5aded6bb683b1de64b7348f
Mark Glushko <markableglushko@gmail.com>
2018-10-15 11:54:50 +0300

    Начало ветки

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticSquadList.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticSquadList.java
@@ -41,7 +41,7 @@ public class FragmentStatisticSquadList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_statistic_squad, container, false);
-
+        Log.i(TAG, "onCreateView: Новая ветка");
         table = view.findViewById(R.id.table_squad);
         return view;
     }
