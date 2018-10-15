3677aeabda4bdcc86f1ddf50a705ac5b7dd842bf
Mark Glushko <markableglushko@gmail.com>
2018-10-15 20:55:08 +0300

    прием массива, переделка в новый и использование сортировки в новых массивах

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticAssistant.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticAssistant.java
@@ -1,19 +1,43 @@
 package com.example.markable.footballapptest.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.markable.footballapptest.Classes.Player;
 import com.example.markable.footballapptest.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class FragmentStatisticAssistant extends Fragment {
 
-    public static FragmentStatisticAssistant  newInstance(){
-        FragmentStatisticAssistant fragmnet = new FragmentStatisticAssistant();
+    private static final String TAG = "FragAssist";
 
-        return fragmnet;
+    private ArrayList<Player> arrayPlayers;
+    private ArrayList<Player> newArrayPlayers = new ArrayList<>();
+
+    public static FragmentStatisticAssistant  newInstance(ArrayList<Player> arrayList){
+        FragmentStatisticAssistant fragment = new FragmentStatisticAssistant();
+        Bundle args = new Bundle();
+        args.putSerializable("arrayPlayers", arrayList);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        arrayPlayers = (ArrayList<Player>) getArguments().getSerializable("arrayPlayers");
+        for(int i=0; i<arrayPlayers.size(); i++){
+            newArrayPlayers.add(arrayPlayers.get(i));
+        }
+        Collections.sort(newArrayPlayers, Player.sortAssist);
+        Log.i(TAG, "onCreate: длина массива =  " + newArrayPlayers.toString());
     }
 
     @Override
--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticBombardier.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentStatisticBombardier.java
@@ -1,21 +1,49 @@
 package com.example.markable.footballapptest.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
 
+import com.example.markable.footballapptest.Classes.Player;
 import com.example.markable.footballapptest.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class FragmentStatisticBombardier extends Fragment {
 
+    private static final String TAG = "FragBombar";
+
+    private ArrayList<Player> arrayPlayers;
+
+    private ArrayList<Player> newArrayPlayers = new ArrayList<>();
+
     TableLayout table;
 
-    public static FragmentStatisticBombardier newInstance(){
+    public static FragmentStatisticBombardier newInstance(ArrayList<Player> arrayList){
         FragmentStatisticBombardier fragment = new FragmentStatisticBombardier();
+        Bundle args = new Bundle();
+        args.putSerializable("arrayPlayersBombardier", arrayList);
+        fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        arrayPlayers = (ArrayList<Player>) getArguments().getSerializable("arrayPlayersBombardier");
+        for(int i=0; i<arrayPlayers.size(); i++){
+            newArrayPlayers.add(arrayPlayers.get(i));
+        }
+        Collections.sort(newArrayPlayers, Player.sortGoal);
+        Log.i(TAG, "onCreate: длина массива =  " + newArrayPlayers.toString());
     }
 
     @Override
@@ -23,8 +51,9 @@ public class FragmentStatisticBombardier extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_statistic_bombardier, container, false);
         table = view.findViewById(R.id.tableForBombardier);
-
+        Log.i(TAG, "onCreateView: новая ветка");
         return view;
     }
+
 
 }
