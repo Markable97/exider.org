e7157616aaa40b201539d1b0439e732eea907bda
Mark Glushko <markableglushko@gmail.com>
2018-10-16 20:35:53 +0300

    Редактировал класс для взаимодействия с ним

--- app/src/main/java/com/example/markable/footballapptest/Classes/AllMatchesForTeam.java
+++ app/src/main/java/com/example/markable/footballapptest/Classes/AllMatchesForTeam.java
@@ -1,4 +1,30 @@
 package com.example.markable.footballapptest.Classes;
 
 public class AllMatchesForTeam {
+
+    PrevMatches matches;
+    String urlImageHome;
+    String urlImageGuest;
+
+    public AllMatchesForTeam(String nameDivision, int idTour, String teamHome,
+                                 int goalHome, int goalVisit, String teamVisit, String urlImageHome, String urlImageGuest) {
+        matches = new PrevMatches(nameDivision, idTour, teamHome, goalHome, goalVisit, teamVisit);
+        this.urlImageHome = urlImageHome;
+        this.urlImageGuest = urlImageGuest;
+    }
+
+    public String getUrlImageHome() {
+        return urlImageHome;
+    }
+
+    public String getUrlImageGuest() {
+        return urlImageGuest;
+    }
+
+    @Override
+    public String toString() {
+        return "AllMatches{" + "nameDivision=" + matches.nameDivision + ", idTour=" + matches.idTour + ", teamHome=" + matches.teamHome +
+                ", goalHome=" + matches.goalHome + ", goalVisit=" + matches.goalVisit + ", teamVisit=" + matches.teamVisit +
+                " urlImageHome=" + this.urlImageHome + " urlImageGuest=" + this.urlImageGuest + "}\n";
+    }
 }
