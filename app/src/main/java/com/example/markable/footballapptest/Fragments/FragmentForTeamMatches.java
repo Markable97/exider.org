73aa9b8dac309bd95bca2b637a7c3de29143aaa1
Mark Glushko <markableglushko@gmail.com>
2018-10-16 23:32:14 +0300

    прием данных из активности

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamMatches.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamMatches.java
@@ -4,22 +4,26 @@ import android.os.Bundle;
 
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.markable.footballapptest.Classes.AllMatchesForTeam;
 import com.example.markable.footballapptest.R;
+
+import java.util.ArrayList;
 
 public class FragmentForTeamMatches extends Fragment {
 
 
     private static final String TAG = "FragMatches";
+    private ArrayList<AllMatchesForTeam> arrayAllMatches = new ArrayList<>();
 
-    public static FragmentForTeamMatches newInstance(){
+    public static FragmentForTeamMatches newInstance(ArrayList<AllMatchesForTeam> list){
         FragmentForTeamMatches fragment = new FragmentForTeamMatches();
-
         Bundle args = new Bundle();
-
+        args.putSerializable("allMatches", list);
         fragment.setArguments(args);
         return fragment;
     }
@@ -27,7 +31,8 @@ public class FragmentForTeamMatches extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        arrayAllMatches = (ArrayList<AllMatchesForTeam>) getArguments().getSerializable("allMatches");
+        Log.i(TAG, "onCreate: длинна массива " + arrayAllMatches.size());
     }
 
     @Override
