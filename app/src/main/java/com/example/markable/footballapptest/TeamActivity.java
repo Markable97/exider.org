83d67a0d9c66906ef7b328c2b428068b48f445e6
Mark Glushko <markableglushko@gmail.com>
2018-10-16 23:33:57 +0300

    загрузка экземплра фрагмента после загрузки данных

--- app/src/main/java/com/example/markable/footballapptest/TeamActivity.java
+++ app/src/main/java/com/example/markable/footballapptest/TeamActivity.java
@@ -72,7 +72,7 @@ public class TeamActivity extends AppCompatActivity implements RadioGroup.OnChec
         fragStatistic = new FragmentForTeamStatistic().newInstance(image.getNameImage());
         fragMatches = new FragmentForTeamMatches();
 
-        fragMatches = new FragmentForTeamMatches().newInstance();
+
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.container_frag_team, fragStatistic ).commit();
 
@@ -139,6 +139,7 @@ public class TeamActivity extends AppCompatActivity implements RadioGroup.OnChec
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            fragMatches = new FragmentForTeamMatches().newInstance(arrayAllMatches);
         }
     }
 
