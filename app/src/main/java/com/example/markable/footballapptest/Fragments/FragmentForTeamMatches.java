af84e36d3effff9d935636157f0e5a26484ad81e
Mark Glushko <markableglushko@gmail.com>
2018-10-16 23:38:31 +0300

    логи на ЖЦ

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamMatches.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamMatches.java
@@ -43,4 +43,40 @@ public class FragmentForTeamMatches extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG,"OnStart ");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "OnResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause" );
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop" );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i(TAG, "onDestroyView");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i(TAG, "onDetach");
+    }
 }
