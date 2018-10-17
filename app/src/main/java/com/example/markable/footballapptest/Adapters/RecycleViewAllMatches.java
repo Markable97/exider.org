4514f9cef26c6b83515f755969c1c5de37110eec
Mark Glushko <markableglushko@gmail.com>
2018-10-17 12:27:16 +0300

    пустой макет для RecycleView

--- app/src/main/java/com/example/markable/footballapptest/Adapters/RecycleViewAllMatches.java
+++ app/src/main/java/com/example/markable/footballapptest/Adapters/RecycleViewAllMatches.java
@@ -1,4 +1,29 @@
 package com.example.markable.footballapptest.Adapters;
 
-public class RecycleViewAllMatches {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecycleViewAllMatches extends RecyclerView.Adapter<RecycleViewAllMatches.ViewHolder> {
+
+    @Override
+    public RecycleViewAllMatches.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecycleViewAllMatches.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
