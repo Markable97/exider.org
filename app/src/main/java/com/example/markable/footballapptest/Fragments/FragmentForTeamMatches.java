b5911e100b02dc85ff4ef75e58cab812ac32808d
Mark Glushko <markableglushko@gmail.com>
2018-10-18 16:40:54 +0300

    Дабавление адаптера в фрагмент

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamMatches.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamMatches.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.markable.footballapptest.Adapters.RecycleViewAllMatches;
 import com.example.markable.footballapptest.Classes.AllMatchesForTeam;
 import com.example.markable.footballapptest.R;
 
@@ -22,11 +24,13 @@ public class FragmentForTeamMatches extends Fragment {
     private ArrayList<AllMatchesForTeam> arrayAllMatches = new ArrayList<>();
 
     RecyclerView recyclerView;
+    RecycleViewAllMatches adapter;
 
     public static FragmentForTeamMatches newInstance(ArrayList<AllMatchesForTeam> list){
         FragmentForTeamMatches fragment = new FragmentForTeamMatches();
         Bundle args = new Bundle();
         args.putSerializable("allMatches", list);
+        Log.i(TAG, "newInstance: " + list.toString());
         fragment.setArguments(args);
         return fragment;
     }
@@ -43,7 +47,9 @@ public class FragmentForTeamMatches extends Fragment {
         View view = inflater.inflate(R.layout.fragment_team_matches, container, false);
 
         recyclerView = view.findViewById(R.id.listAllMatches);
-
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new RecycleViewAllMatches(arrayAllMatches);
+        recyclerView.setAdapter(adapter);
 
 
         return view;
