98f98e30f3f780a3cc2e3cd6c133187d53589efa
Mark Glushko <markableglushko@gmail.com>
2018-10-21 10:31:59 +0300

    Перенос класса загрузки в главную активность

--- app/src/main/java/com/example/markable/footballapptest/MainActivity.java
+++ app/src/main/java/com/example/markable/footballapptest/MainActivity.java
@@ -16,13 +16,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.markable.footballapptest.Classes.ImageFromServer;
+import com.example.markable.footballapptest.Classes.NextMatches;
+import com.example.markable.footballapptest.Classes.PrevMatches;
+import com.example.markable.footballapptest.Classes.TournamentTable;
 import com.example.markable.footballapptest.Fragments.FragmentMain;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    final String IP = "10.0.2.2";
+
     private static final String TAG = "MainAct";
     FragmentMain fragmentMain;
+
+    private ArrayList<ImageFromServer> imageArray = new ArrayList<>();
+    private ArrayList<TournamentTable> tournamentTable = new ArrayList<>();
+    private ArrayList<PrevMatches> prevResultsMatch = new ArrayList<>();
+    private ArrayList<NextMatches> nextResultsMatch = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,16 +157,90 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public class MainServerConnect extends AsyncTask<Void, String, Void>{
+    public class MainServerConnect extends AsyncTask<String, Void, String>{
+
+        //String query = "{\"id_division\":1,\"id_tour\":2}";
+        String query = "";
+        String fromServer = null, fromServerResultsPrevMatches = null, fromServerCalendar = null ;
+        //String ipAdres = "192.168.0.104";
+        //String ipAdres = "92.38.241.107";
+        //String ipAdres = "10.0.2.2"
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected String doInBackground(String... strings) {
+
+            for(String s : strings){
+                query = "{\"messageLogic\":\"division\",\"id_division\":"+ s + "}";
+                //query = "{\"id_division\":" + s + ",\"id_tour\":2}";
+            }
+
+            Log.i(TAG, "Поток запущен");
+            Socket socket;
+            Gson gson = new Gson();
+            try {
+                socket = new Socket(IP, 55555);
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                //DataInputStream inResultsPrev = new DataInputStream((socket.getInputStream()));
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                out.writeUTF(query);
+
+                fromServer = in.readUTF();
+                Log.i(TAG, "Данные с сервера в виду JSON = " + fromServer);
+                tournamentTable.clear();
+                tournamentTable = gson.fromJson(fromServer, new TypeToken<ArrayList<TournamentTable>>(){}.getType());
+                for(int i = 0; i<tournamentTable.size(); i++){
+                    Log.i(TAG, "doInBackground: " + tournamentTable.get(i).toString());
+                }
+                fromServerResultsPrevMatches = in.readUTF();
+                prevResultsMatch.clear();
+                prevResultsMatch = gson.fromJson(fromServerResultsPrevMatches, new TypeToken<ArrayList<PrevMatches>>(){}.getType());
+                Log.i(TAG,"[2] Данные с сервера в виде JSON = " + fromServerResultsPrevMatches);
+                for(int i = 0; i<prevResultsMatch.size(); i++){
+                    Log.i(TAG, "doInBackground: " + prevResultsMatch.get(i).toString());
+                }
+                fromServerCalendar = in.readUTF();
+                nextResultsMatch.clear();
+                nextResultsMatch = gson.fromJson(fromServerCalendar, new TypeToken<ArrayList<NextMatches>>(){}.getType());
+                Log.i(TAG, "[3] Данные с сервера в виде JSON = " + fromServerCalendar);
+                Log.i(TAG, nextResultsMatch.toString());
+                imageArray.clear();
+                int countFiles = in.readInt();
+                byte[] byteArrayBig;
+                Log.i(TAG, "doInBackground ServerTest: Кол-во файлов " + countFiles);
+                for(int i = 0; i < countFiles; i++){
+                    String nameImageFromServer = in.readUTF();
+                    Log.i(TAG, "doInBackground: название картинки = " + nameImageFromServer);
+                    /*int countBytes = in.readInt();
+                    Log.i(TAG, "doInBackground: кол-во байтов = " + countBytes );
+                    byte[] byteArray = new byte[countBytes];
+                    //int countFromServer = in.read(byteArray, 0, countBytes);
+                    in.readFully(byteArray);
+                    Log.i(TAG, "doInBackground: размер массива байтов " + byteArray.length);*/
+                    int countBytesBig = in.readInt();
+                    Log.i(TAG, "doInBackground: кол-во байтов большой картинки" + countBytesBig);
+                    byteArrayBig = new byte[countBytesBig];
+                    in.readFully(byteArrayBig);
+                    Log.i(TAG, "doInBackground: размер массива большой картинки байтов " + byteArrayBig.length);
+                    imageArray.add(new ImageFromServer(nameImageFromServer,/*
+                            BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length),*/
+                            byteArrayBig));
+                }
+                Log.i(TAG, "doInBackground: ImageFromServer = " + imageArray.size());
+
+                out.writeUTF("{\"messageLogic\":\"close\"}");
+                out.close();
+                in.close();
+                //inResultsPrev.close();
+                socket.close();
+
+/* я крокодил, крокожу и буду крокодить */
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             return null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
         }
     }
     @Override
