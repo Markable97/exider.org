c5764054645d273fd5de3912f39db29fbc5f0ba3
Mark Glushko <markableglushko@gmail.com>
2018-10-22 14:08:56 +0300

    Убрал класс загрузки, перенес в активность. Ссылки из активности а не через getArguments

--- app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamStatistic.java
+++ app/src/main/java/com/example/markable/footballapptest/Fragments/FragmentForTeamStatistic.java
@@ -19,6 +19,7 @@ import com.example.markable.footballapptest.Adapters.FragmentPageAdapterForStati
 import com.example.markable.footballapptest.Classes.Player;
 import com.example.markable.footballapptest.R;
 import com.example.markable.footballapptest.ReturnFromFragForAct;
+import com.example.markable.footballapptest.TeamActivity;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -31,6 +32,7 @@ import java.util.ArrayList;
 
 public class FragmentForTeamStatistic extends Fragment {
 
+    TeamActivity activity;
 
     private static final String TAG = "FragStatistic";
 
@@ -46,21 +48,19 @@ public class FragmentForTeamStatistic extends Fragment {
     ArrayList<Player> arrayPlayers = new ArrayList<>();
     String teamName;
 
-    public static FragmentForTeamStatistic newInstance(String name){
+    /*public static FragmentForTeamStatistic newInstance(String name){
         Log.i(TAG, "NewInstance: " + name);
         FragmentForTeamStatistic fragment = new FragmentForTeamStatistic();
         Bundle args = new Bundle();
         args.putString("nameTeam", name);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
-    public static FragmentForTeamStatistic newInstance(ArrayList<Player> player){
-        Log.i(TAG, "NewInstance2: " + player.toString());
+    public static FragmentForTeamStatistic newInstance(){
+
         FragmentForTeamStatistic fragment = new FragmentForTeamStatistic();
-        Bundle args = new Bundle();
-        args.putSerializable("nameTeamArray", player);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -75,17 +75,16 @@ public class FragmentForTeamStatistic extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String fromBun = getArguments().getString("nameTeam","хуй тебе а не данные");
-        Log.i(TAG, "onCreate: " + fromBun);
-        //Log.i(TAG, "onCreate: " + arrayPlayers.size());
-        if(getArguments().getSerializable("nameTeamArray" ) == null){
+
+        Log.i(TAG, "onCreate: " );
+       /* if(getArguments().getSerializable("nameTeamArray" ) == null){
             Log.i(TAG, "onCreate: длина массива равна 0");
             new ServerConnect().execute(fromBun);
         }else {
-            arrayPlayers = (ArrayList<Player>) getArguments().getSerializable("nameTeamArray");
+            arrayPlayers = activity.getArrayPlayers();
             Log.i(TAG, "onCreate: Длина массива = " + arrayPlayers.size() );
             flag = true;
-        }
+        }*/
 
 
     }
@@ -93,82 +92,19 @@ public class FragmentForTeamStatistic extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_team_statistic, container, false);
-
+        activity = (TeamActivity)getActivity();
         Log.i(TAG, "onCreateView: ");
-
+        arrayPlayers = activity.getArrayPlayers();
         viewPager = view.findViewById(R.id.viewPager_team_statistic);
         tabLayout = view.findViewById(R.id.statistic_tabs);
-        if(flag){
-            fromFragForAct.sendDataToActivity(arrayPlayers);
-            mAdapter = new FragmentPageAdapterForStatistic(getChildFragmentManager(), arrayPlayers);
-            viewPager.setAdapter(mAdapter);
-            tabLayout.setupWithViewPager(viewPager);
-        }
+        fromFragForAct.sendDataToActivity(arrayPlayers);
+        mAdapter = new FragmentPageAdapterForStatistic(getChildFragmentManager(), arrayPlayers);
+        viewPager.setAdapter(mAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
         return view;
     }
-
-    public class ServerConnect extends AsyncTask<String, Void, String>{
-
-        String queryClose = "{\"messageLogic\":\"close\"}";
-        String query = "";
-        //final String IP = "10.0.2.2";
-        final String IP = "192.168.0.105";
-        String fromServer = null;
-
-        @Override
-        protected String doInBackground(String... strings) {
-            Log.i(TAG, "doInBackground: поток запущен");
-            for(String s : strings){
-                query = "{\"messageLogic\":\"team\",\"id_team\":\""+ s + "\"}";
-                //query = "{\"id_division\":" + s + ",\"id_tour\":2}";
-            }
-            Socket socket;
-            Gson gson = new Gson();
-
-            try {
-                socket = new Socket(IP, 55555);
-                DataInputStream in = new DataInputStream(socket.getInputStream());
-                //DataInputStream inResultsPrev = new DataInputStream((socket.getInputStream()));
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-                out.writeUTF(query);
-
-                fromServer = in.readUTF();
-                Log.i(TAG, "doInBackground: Данные от сервера" + fromServer);
-                Type t = new TypeToken<ArrayList<Player>>(){}.getType();
-                arrayPlayers = gson.fromJson(fromServer, t);
-                Log.i(TAG, "doInBackground: \n" + arrayPlayers.toString());
-
-                int countImage = in.readInt();
-                Log.i(TAG, "doInBackground: кол-во фоток от сервера = " + countImage);
-                if(countImage != 0 && countImage==arrayPlayers.size()){
-                    byte[] byteArray;
-                    for(int i = 0; i < countImage; i++){
-                        int countBytes = in.readInt();
-                        byteArray = new byte[countBytes];
-                        in.readFully(byteArray);
-                        arrayPlayers.get(i).setPlayerImage(BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length));
-                    }
-                }
-
-                out.writeUTF(queryClose);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            fromFragForAct.sendDataToActivity(arrayPlayers);
-            mAdapter = new FragmentPageAdapterForStatistic(getChildFragmentManager(), arrayPlayers);
-            viewPager.setAdapter(mAdapter);
-            tabLayout.setupWithViewPager(viewPager);
-        }
-    }
+    
 
     @Override
     public void onStart() {
