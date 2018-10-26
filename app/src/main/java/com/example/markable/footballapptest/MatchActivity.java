ef7ee5edbeb800f84454fa8147bb6e87aea68209
Mark Glushko <markableglushko@gmail.com>
2018-10-26 17:38:53 +0300

    Загркзеа в макет данныъ из интента

--- app/src/main/java/com/example/markable/footballapptest/MatchActivity.java
+++ app/src/main/java/com/example/markable/footballapptest/MatchActivity.java
@@ -3,6 +3,8 @@ package com.example.markable.footballapptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.markable.footballapptest.Classes.ImageFromServer;
 import com.example.markable.footballapptest.Classes.PrevMatches;
@@ -15,6 +17,15 @@ public class MatchActivity extends AppCompatActivity {
     ImageFromServer imageVisit;
     PrevMatches matches;
 
+    TextView division;
+    TextView tour;
+    TextView count;
+    TextView nameTeamHome;
+    TextView nameTeamVisit;
+    ImageView imageTeamHome;
+    ImageView imageTeamVisit;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +37,22 @@ public class MatchActivity extends AppCompatActivity {
             imageVisit = args.getParcelable("imageVisit");
             Log.i(TAG, "onCreate: " + matches.toString() + imageVisit.getNameImage() + imageHome.getNameImage());
         }
+
+        division = findViewById(R.id.match_nameDivision);
+        division.setText(matches.getNameDivision());
+        tour = findViewById(R.id.match_tour);
+        tour.setText("Тур " + String.valueOf(matches.getIdTour()));
+        count = findViewById(R.id.match_count);
+        count.setText(String.valueOf(matches.getGoalHome()) + " : " + String.valueOf(matches.getGoalVisit()));
+        nameTeamHome = findViewById(R.id.match_teamHome);
+        nameTeamHome.setText(matches.getTeamHome());
+        nameTeamVisit = findViewById(R.id.match_teamGuest);
+        nameTeamVisit.setText(matches.getTeamVisit());
+        imageTeamHome = findViewById(R.id.match_imageHome);
+        imageTeamHome.setImageBitmap(imageHome.getBitmapImageBig());
+        imageTeamVisit = findViewById(R.id.match_imageGuest);
+        imageTeamVisit.setImageBitmap(imageVisit.getBitmapImageBig());
+
     }
 
     @Override
