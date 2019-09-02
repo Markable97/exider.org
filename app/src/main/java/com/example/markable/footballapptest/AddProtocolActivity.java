47b22e6d9a38b2463df8d45661cddf7bc7df1168
Markable97 <markableglushko@gmail.com>
2019-09-02 17:05:56 +0300

    Добавление тулбара и првеление манифеста к красивому виду

--- app/src/main/java/com/example/markable/footballapptest/AddProtocolActivity.java
+++ app/src/main/java/com/example/markable/footballapptest/AddProtocolActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -41,11 +42,14 @@ public class AddProtocolActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     TabLayout tabLayout;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_result);
+        toolbar =  (Toolbar) findViewById(R.id.protocol_toolbar);
+        setSupportActionBar(toolbar);
         tv_division = (TextView)findViewById(R.id.protocol_division);
         //tv_tour = (TextView)findViewById(R.id.protocol_tour);
         tv_teamHome = (TextView) findViewById(R.id.protocol_teamHome);
